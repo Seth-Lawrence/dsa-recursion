0672c165c5f98ae9b3f0656802ee065b48361a0a
Veronica Ni <vni1294@gmail.com>
2024-02-27 18:37:00 -0500

    Working on everyOther()

--- recursion/every-other/everyOther.ts
+++ recursion/every-other/everyOther.ts
@@ -1,7 +1,14 @@
 /** everyOther: return a string with every other letter. */
 
 function everyOther(str: string): string {
-  return "x";
+  if (str.length === 0) return "";
+  console.log(str[0]);
+  everyOther(str.slice(2));
+  // let idx = 0;
+  // const letters: string [] = str.split("");
+  // let curr = letters[idx];
+  // let out = curr += everyOther()
+  // return out;
 }
 
 export { everyOther };
