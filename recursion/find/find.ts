16cec858b89a8f1d693f4e7dfa772f584acd8ab7
Veronica Ni <vni1294@gmail.com>
2024-02-27 19:51:30 -0500

    Finished find

--- recursion/find/find.ts
+++ recursion/find/find.ts
@@ -1,7 +1,12 @@
 /** find: return t/f is val is in arr. */
-
+//[] -> false
+//[3] -> false
+//[2, 3] -> true
+//[1, 2, 3] --> true
 function find(arr: number[], val: number): boolean {
-  return true;
+  if (arr.length === 0) return false;
+  if (arr[0] === val) return true;
+  return (find(arr.slice(1), val));
 }
 
 export { find };
