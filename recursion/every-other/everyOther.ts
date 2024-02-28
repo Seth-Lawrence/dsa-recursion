7d1e973ab7ca6affab34353b2654eb62fea813f9
Veronica Ni <vni1294@gmail.com>
2024-02-27 19:01:55 -0500

    finished everyOther

--- recursion/every-other/everyOther.ts
+++ recursion/every-other/everyOther.ts
@@ -2,13 +2,8 @@
 
 function everyOther(str: string): string {
   if (str.length === 0) return "";
-  console.log(str[0]);
-  everyOther(str.slice(2));
-  // let idx = 0;
-  // const letters: string [] = str.split("");
-  // let curr = letters[idx];
-  // let out = curr += everyOther()
-  // return out;
+  let out = (str[0]) + everyOther(str.slice(2));
+  return out;
 }
 
 export { everyOther };
